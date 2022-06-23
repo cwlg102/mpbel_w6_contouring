018306bbc47c1866955be7836c841211aafc72db
larmor freq <100037095+cwlg102@users.noreply.github.com>
2022-06-23 14:44:08 +0900

    Add files via upload

--- Contouring3dadvanced.py
+++ Contouring3dadvanced.py
@@ -1,7 +1,7 @@
 import pydicom
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+
 dcm = pydicom.dcmread('RTs.dcm')
 all_ctr = dcm.ROIContourSequence #ROIContourSequence로 Contour 좌표 불러올 수 있음.
 
@@ -24,11 +24,11 @@ while sdx < 21: #len이 안먹혀서
         except: xiplus, yiplus, ziplus = float(ctr_coord_1dim[0]), float(ctr_coord_1dim[1]), float(ctr_coord_1dim[2])
         #####예외, xi, yi, zi가 마지막 일경우, 그냥 pass하면 안되고 첫번째 점과 연결을 시켜주어야함.#####
 
-        #largediff = max([abs(xi-xiplus), abs(yi-yiplus), abs(zi-ziplus)])
+        
         largedist = ((xi - xiplus)**2 + (yi - yiplus)**2 + (zi - ziplus)**2) ** 0.5
         if largedist < 1: #만약 길이 차이가 적게나서 1보다 작을 때 largedist를 기본 내분 갯수에 곱하면 내분 갯수가 기본보다 작아짐.
             largedist = 1
-        indiv = int(20 * largedist) #distance에 따라 weight 가 달라짐
+        indiv = int(5 * largedist) #distance에 따라 weight 가 달라짐
         
         for jdx in range(1, indiv):
             #내분점 공식
@@ -41,14 +41,21 @@ while sdx < 21: #len이 안먹혀서
         adx += 1
 
     sdx += 1 #다음 슬라이스로
+    coord_arr = np.delete(coord_arr, 0, axis = 0) #zeros로 만들었으니, 맨앞은 0, 0, 0이라 삭제해주어야함.
     ctr_volume.append(coord_arr)
 
 x = []
 y = []   
 z = []  
-flag = 0
+#print('1 : 3D로 출력, 0 : 2D로 출력: ', end ='')
+#button = int(input())
+#if button == 1:
+#    iwant3d = 1
+#else:iwant3d = 0
+iwant3d = 1
 
-if flag == 0:
+
+if iwant3d == 0:
     ctrx = []
     ctry = []
     
@@ -67,19 +74,18 @@ if flag == 0:
         plt.figure()
         plt.axis((-255, 256, -511, 0))
         plt.scatter(ctrx[_slice], ctry[_slice], s = 1)
-    plt.show()
+    plt.imshow()
 
-elif flag == 1:
+elif iwant3d == 1:
     fig = plt.figure(figsize = (6, 6))
     ax = fig.add_subplot(projection = '3d')
     for k in range(len(ctr_volume)):
         for i in ctr_volume[k]:
-            if i[0] == 0 and i[1] == 0 and i[2] == 0:
-                continue
+            
             x.append(i[0])
             y.append(i[1])
             z.append(i[2])
     ax.axis((-50, 50, -300, -200))
-    ax.scatter(x, y, z, c = 'magenta', s = 1)
+    ax.scatter(x, y, z, s = 1)
     ax.set_zlim(-300, -150)
     plt.show()
